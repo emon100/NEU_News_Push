c75871390a06560eadb49dc0535a63f1d6435da9
emon100 <emon100@qq.com>
2020-02-18 01:25:39 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,12 +2,28 @@ const headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36'
 };
 const sitesConfig = {
+    '东大创新网': {
+        type: "html",
+        protocol: "http",
+        siteHost: "http://cxzx.neu.edu.cn/main.htm",
+        parts: {
+            '通知公告': {
+                selector: ['#tzlist li:first-child div']
+            }
+        }
+    },
     '东大教务处官网': {
         //type有html和json两种。
         type: "html",
         protocol: "http",
         siteHost: "http://aao.neu.edu.cn/",
         parts: {
+            '通知': {
+                selector: ['[frag="窗口51"] div:first-child+div span font']
+            },
+            '公告': {
+                selector: ['[frag="窗口6"] div:first-child+div']
+            },
             '教学研究': {
                 //cheerio的dom元素选择器语法，类似jQuery的选择器和CSS Selector语法
                 selector: ['[frag="窗口9"] li:first-child']
@@ -17,8 +33,7 @@ const sitesConfig = {
     '计算机学院官网': {
         type: "html",
         protocol: "http",
-        siteUrl: "www.cse.neu.edu.cn",
-        path: '/',
+        siteUrl: "http://www.cse.neu.edu.cn/",
         parts: {
             '通知公告': {
                 //自己定义的json反序列化之后的对象的处理函数，输出字符串。
