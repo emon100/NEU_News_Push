e70490a4b0fc99e6fe90a35ffc1e3bd74f76310d
emon100 <emon100@qq.com>
2020-02-20 21:37:12 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+const https = require('https');
 const headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36'
 };
